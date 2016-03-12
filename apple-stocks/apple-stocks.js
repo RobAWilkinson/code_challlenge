5ed25536b0263af89597fb1d65a7de63dd6e61b0
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2016-03-12 12:44:53 -0800

    inital commit

---
+++ apple-stocks/apple-stocks.js
@@ -0,0 +1,18 @@
+/**
+ *  I have an array stock_prices_yesterday where:
+ *
+ *    - The indices are the time in minutes past trade opening time, which was 9:30am local time
+ *    - The values are the prices in dollars of Apple stock at the time
+ *
+ *  For example, the stock cost $500 at 10:30am, so stock_prices_yesterday[60] = 500;
+ *
+ *  Write an efficient algorithm for computing the best profit I could have made from 1 purchase
+ *  and 1 sale of 1 Apple stock yesterday
+ *
+ */
+
+function bestProfit(stock_prices_yesterday) {
+
+}
+
+module.exports = bestProfit;
